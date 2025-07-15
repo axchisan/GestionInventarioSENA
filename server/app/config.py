0825c21547368164d27402cd1966375bf1d89afc
axchisan <leftyrancuentabot@gmail.com>
2025-07-14 21:58:55 -0500

    Remove sensitive .env file

--- server/app/config.py
+++ server/app/config.py
@@ -1,22 +1,22 @@
 from pydantic_settings import BaseSettings # type: ignore
 
 class Settings(BaseSettings):
-    DATABASE_HOST: str = "147.93.178.204"
-    DATABASE_PORT: str = "5433"
-    DATABASE_USER: str = "postgres"
-    DATABASE_PASSWORD: str = "F2lCLAg76EfRwbd1mkTnC2ZdEuwp8teHyADb7n23YomSk2VI4CipeyW21XZLApIe"
-    DATABASE_NAME: str = "gestionInventario"
+    DATABASE_HOST: str
+    DATABASE_PORT: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_NAME: str
     DATABASE_URL: str = None # type: ignore
 
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    APP_PORT: int = 8001  # Puerto ajustado para evitar conflicto con Coolify
+    APP_PORT: int = 8001
 
     class Config:
-        env_file = ".env"
+        env_file = ".env"  
         env_file_encoding = "utf-8"
-        extra = "ignore"  # Ignora campos no definidos como source_commit y host
+        extra = "ignore"  
 
     def __init__(self, **data):
         super().__init__(**data)
