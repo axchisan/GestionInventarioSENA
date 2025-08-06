f8c36352b72edc1af1e271f72847c583d11e52ce
axchisan <leftyrancuentabot@gmail.com>
2025-08-05 19:37:23 -0500

    cambios en los id de la base de datos a uuid nuevamente

--- client/lib/core/constants/api_constants.dart
+++ client/lib/core/constants/api_constants.dart
@@ -1,4 +1,4 @@
-const String baseUrl = 'http://147.93.178.204:8001/api/auth';
+const String baseUrl = 'https://senainventario.axchisan.com/api/auth';
 const String loginEndpoint = '/api/auth/login';
 const String registerEndpoint = '/api/auth/register';
 const String getUserEndpoint = '/api/auth/me';
