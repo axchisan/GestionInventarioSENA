6de4271c83128ab8d1796163cc9d1ad4de0c3930
axchisan <leftyrancuentabot@gmail.com>
2025-08-08 17:56:37 -0500

    se ajusto la ruta de la api para el despliegue

--- client/lib/core/constants/api_constants.dart
+++ client/lib/core/constants/api_constants.dart
@@ -1,4 +1,4 @@
-const String baseUrl = 'http://0.0.0.0:8001/api/auth';
+const String baseUrl = 'https://senainventario.axchisan.com/api/auth';
 const String loginEndpoint = '/api/auth/login';
 const String registerEndpoint = '/api/auth/register';
 const String getUserEndpoint = '/api/auth/me';
