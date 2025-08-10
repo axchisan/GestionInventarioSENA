0db8c4c217ed6e12a5317b711ebec6974aa5f381
axchisan <leftyrancuentabot@gmail.com>
2025-08-10 01:27:50 -0500

    rutas correjidas en api_constantsdart

--- client/lib/core/constants/api_constants.dart
+++ client/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
-const String baseUrl = 'https://senainventario.axchisan.com'; 
-const String loginEndpoint = '/api/auth/api/auth/login';
-const String registerEndpoint = '/api/auth/api/auth/register';
-const String getUserEndpoint = '/api/auth/api/auth/me';
-const String environmentsEndpoint = '/api/environments/api/environments'; 
-const String inventoryEndpoint = '/api/inventory/api/inventory'; 
-const String qrGenerateEndpoint = '/api/qr/api/qr/generate'; 
+const String baseUrl = 'https://senainventario.axchisan.com';
+const String loginEndpoint = '/api/auth/login';
+const String registerEndpoint = '/api/auth/register';
+const String getUserEndpoint = '/api/auth/me';
+const String environmentsEndpoint = '/api/environments';
+const String inventoryEndpoint = '/api/inventory';
+const String qrGenerateEndpoint = '/api/qr/generate';
