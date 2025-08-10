cbd7fa9614389ab2dfe061c163bf11438a9b693a
axchisan <leftyrancuentabot@gmail.com>
2025-08-09 23:31:51 -0500

    preparacion de rutas otra vez para despliegue

--- client/lib/core/constants/api_constants.dart
+++ client/lib/core/constants/api_constants.dart
@@ -1,4 +1,4 @@
-const String baseUrl = 'http://0.0.0.0:8001'; 
+const String baseUrl = 'https://senainventario.axchisan.com'; 
 const String loginEndpoint = '/api/auth/api/auth/login';
 const String registerEndpoint = '/api/auth/api/auth/register';
 const String getUserEndpoint = '/api/auth/api/auth/me';
